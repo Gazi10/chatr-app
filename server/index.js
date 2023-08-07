737c308248f1cf7ffa9949210424342b6a87f6b4
Gazi <62996216+Gazi10@users.noreply.github.com>
2023-08-07 13:36:51 -0400

    implement auth

---
+++ server/index.js
@@ -0,0 +1,127 @@
+import express from 'express';
+import mongoose from "mongoose";
+import 'dotenv/config'
+import cors from "cors";
+import { OAuth2Client } from "google-auth-library";
+import jwt from "jsonwebtoken";
+import ChatRoute from './routes/ChatRoute.js';
+
+const app = express();
+const PORT = 8000;
+let DB = [];
+// const uri = "mongodb+srv://gazi:<cs326>@chatr.jwbbusf.mongodb.net/?retryWrites=true&w=majority";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+  })
+);
+
+app.use(express.json());
+
+async function verifyGoogleToken(token) {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    return { payload: ticket.getPayload() };
+  } catch (error) {
+    return { error: "Invalid user detected. Please try again" };
+  }
+}
+
+app.post("/signup", async (req, res) => {
+  try {
+    // console.log({ verified: verifyGoogleToken(req.body.credential) });
+    if (req.body.credential) {
+      const verificationResponse = await verifyGoogleToken(req.body.credential);
+
+      if (verificationResponse.error) {
+        return res.status(400).json({
+          message: verificationResponse.error,
+        });
+      }
+
+      const profile = verificationResponse?.payload;
+
+      DB.push(profile);
+
+      res.status(201).json({
+        message: "Signup was successful",
+        user: {
+          firstName: profile?.given_name,
+          lastName: profile?.family_name,
+          picture: profile?.picture,
+          email: profile?.email,
+          token: jwt.sign({ email: profile?.email }, "myScret", {
+            expiresIn: "1d",
+          }),
+        },
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occurred. Registration failed.",
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    if (req.body.credential) {
+      const verificationResponse = await verifyGoogleToken(req.body.credential);
+      if (verificationResponse.error) {
+        return res.status(400).json({
+          message: verificationResponse.error,
+        });
+      }
+
+      const profile = verificationResponse?.payload;
+
+      const existsInDB = DB.find((person) => person?.email === profile?.email);
+
+      if (!existsInDB) {
+        return res.status(400).json({
+          message: "You are not registered. Please sign up",
+        });
+      }
+
+      res.status(201).json({
+        message: "Login was successful",
+        user: {
+          firstName: profile?.given_name,
+          lastName: profile?.family_name,
+          picture: profile?.picture,
+          email: profile?.email,
+          token: jwt.sign({ email: profile?.email }, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+          }),
+        },
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error?.message || error,
+    });
+  }
+});
+
+
+// mongoose
+//   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+//   .catch((error) => console.log(`${error} did not connect`));
+
+// app.get('/',  (req, res) => {
+//     res.send('Welcome');
+// });
+ 
+app.listen(PORT, () => {
+    console.log("Server listening on PORT", PORT);
+});
+
+app.use('/chat', ChatRoute);
